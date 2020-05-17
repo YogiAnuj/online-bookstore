760050e57e90c279a874804a89d40bedf137902b
YogiAnuj <yogianujnath@gmail.com>
2020-05-18 01:15:38 +0530

    completed feature 4 dynamic category

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Book } from '../model/book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ import { Book } from '../model/book';
 export class BookService {
 
   private baseUrl = 'http://localhost:8080/api/v1/book';
+  private categoryUrl = 'http://localhost:8080/api/v1/book-category';
+  
+
   constructor(private httpClient: HttpClient) { }
 
   getBooks(categoryId: number): Observable<Book[]> {
@@ -20,10 +24,22 @@ export class BookService {
       map(response => response._embedded.book)
     );
   }
+
+  getBooksCategory(): Observable<BookCategory[]>{
+    return this.httpClient.get<GetResponseBooksCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    );
+  }
 }
 
 interface GetResponseBooks {
   _embedded: {
     book: Book[];
+  };
+}
+
+interface GetResponseBooksCategory {
+  _embedded: {
+    bookCategory: BookCategory[];
   };
 }
